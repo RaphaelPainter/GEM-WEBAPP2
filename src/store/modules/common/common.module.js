1bd75ee2f230b9db2de6d0c1e946ebfdf4a3bfe3
RaphaelPainter <raphapainter@wanadoo.fr>
2022-01-21 10:04:29 +0100

    construction(store)

--- src/store/modules/common/common.module.js
+++ src/store/modules/common/common.module.js
@@ -1,6 +1,12 @@
 import state from "./common.state"
+import actions from "./common.actions"
+import getters from "./common.getters"
+import mutations from "./common.mutations"
 
 export default {
     namespaced: true,
-    state
+    state,
+    actions,
+    getters,
+    mutations
 }
