82874e0d8de9ed327d2b226c502e41d425a51ced
RaphaelPainter <raphapainter@wanadoo.fr>
2022-01-28 18:10:34 +0100

    setup(axios)

--- src/store/modules/common/common.actions.js
+++ src/store/modules/common/common.actions.js
@@ -1,6 +1,19 @@
+const axios = require('axios').default;
+
+
 export default {
     setApiHealth_action({ commit, dispatch }) {
-        console.log("bla")
-        commit("setApiHealth_mutation", "changed")
+        axios.post('https://gem44-api.herokuapp.com/adherents')
+            .then(function(response) {
+                // handle success
+                commit("setApiHealth_mutation", "changed")
+            })
+            .catch(function(error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function() {
+                // always executed
+            });
     }
 }
--- src/store/modules/common/common.mutations.js
+++ src/store/modules/common/common.mutations.js
@@ -1,8 +1,5 @@
 export default {
     setApiHealth_mutation(state, value) {
-        console.log("bli")
         state.apiHealth = value
-        console.log("blu")
-
     }
 }
